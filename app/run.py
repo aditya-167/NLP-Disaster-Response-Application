1be990942f97d63133711213b13e0148bd89dbcc
Aditya Srichandan <adityasrichandan3098@gmail.com>
2020-08-02 03:30:48 +0530

    final changes

--- app/run.py
+++ app/run.py
@@ -6,7 +6,7 @@ from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk import pos_tag, word_tokenize
 import nltk
-
+import os
 from sklearn.base import BaseEstimator, TransformerMixin
 import joblib
 from flask import Flask
