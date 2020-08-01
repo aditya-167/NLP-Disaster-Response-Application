294bb2c286552baae62df27e94183bfd8d3c96cd
Aditya Srichandan <adityasrichandan3098@gmail.com>
2020-08-02 02:27:41 +0530

    app modify main

--- app/run.py
+++ app/run.py
@@ -136,9 +136,9 @@ def go():
     )
 
 
-#def main():
-    app.run()
+def main():
+    app.run(host='0.0.0.0', port=3001, debug=True)
 
 
-#if __name__ == '__main__':
-#    main()
+if __name__ == '__main__':
+    main()
