d9855c3f2ee7607b03428bbfaa3323f14f89548b
Aditya Srichandan <adityasrichandan3098@gmail.com>
2020-08-02 03:26:20 +0530

    final

--- app/run.py
+++ app/run.py
@@ -140,5 +140,6 @@ def go():
 #    app.run(debug=True)
 
 
-#if __name__ == '__main__':
-#    main()
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
