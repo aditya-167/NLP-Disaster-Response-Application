e69ab84ad52b71c342cb4a8d5e1de8ebafac0395
Aditya Srichandan <adityasrichandan3098@gmail.com>
2020-08-02 11:40:31 +0530

    Procfile final

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
 #from sklearn.externals import joblib
 from sqlalchemy import create_engine
-from .utils import tokenize
+
 
 app = Flask(__name__)
 
